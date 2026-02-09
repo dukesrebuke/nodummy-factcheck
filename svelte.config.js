b19577060493a93ef3829db1ffb81958b5f85ace
dukesrebuke <dukeslevert@gmail.com>
2026-02-09 18:47:32 -0500

    Fix: SvelteKit Netlify adapter configuration

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      // Don't try to append to _headers if it doesn't exist
+      split: false
+    })
+  }
 };
 
 export default config;
