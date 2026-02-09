83080bac46f04d38003194c8bdfb9c3a76c6d266
dukesrebuke <dukeslevert@gmail.com>
2026-02-09 03:22:24 -0500

    fix adapter for netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  kit: {
-    adapter: adapter(),
-    prerender: {
-      default: false
-    }
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
